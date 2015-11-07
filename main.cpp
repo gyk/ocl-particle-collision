389ca7784939bda147cbc3e5c0e771099c40e607
Yukun Guo <gyk.net@gmail.com>
2015-11-07 12:08:45 +0800

    Print out more information

--- main.cpp
+++ main.cpp
@@ -21,9 +21,19 @@ int main(int argc, char const *argv[])
     }
 
     for (int i=0; i<10; i++) {
-       Collision colli = scene.nextCollisionTime();
-       printf("%f: (%i, %i)\n", colli.occurTime, colli.object1, colli.object2);
-       scene.collideAndUpdate(colli);
+        Collision colli = scene.nextCollisionTime();
+        printf("%f: (%i, %i)\n", colli.occurTime, colli.object1, colli.object2);
+        if (!CollisionObject::againstWall(colli.object1) &&
+            !CollisionObject::againstWall(colli.object2)) {
+            auto& p1 = scene.getParticleAt(colli.object1);
+            auto& p2 = scene.getParticleAt(colli.object2);
+            printf("    m1: %f    m2: %f\n", p1.mass, p2.mass);
+            printf("    p1-p2: (%f, %f)    v1-v2: (%f, %f)\n", 
+                p1.position[0] - p2.position[0], p1.position[1] - p2.position[1], 
+                p1.velocity[0] - p2.velocity[0], p1.velocity[1] - p2.velocity[1]);
+        }
+
+        scene.collideAndUpdate(colli);
     }
 
 	return 0;
